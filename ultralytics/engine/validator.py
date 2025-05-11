9c531a3a86e4af728c1616d021b4a7da59bfdb07
Pau Amargant <90143513+pauamargant@users.noreply.github.com>
2025-05-11 15:59:32 +0200

    a

--- ultralytics/engine/validator.py
+++ ultralytics/engine/validator.py
@@ -147,8 +147,7 @@ class BaseValidator:
             self.data = trainer.data
             # Force FP16 val during training
             self.args.half = self.device.type != "cpu" and trainer.amp
-            model = trainer.model
-            # model = trainer.ema.ema or trainer.model
+            model = trainer.ema.ema or trainer.model
             model = model.half() if self.args.half else model.float()
             # self.model = model
             self.loss = torch.zeros_like(trainer.loss_items, device=trainer.device)
